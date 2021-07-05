322a04a032cb64dd8a39fce6b41723ea02426056
radhaSaoji <saojiradha@gmail.com>
2021-07-05 22:36:46 +0530

    TestCase3

--- src/com/technocredits/ornghrm/testScripts/BaseTest.java
+++ src/com/technocredits/ornghrm/testScripts/BaseTest.java
@@ -6,9 +6,12 @@ import org.testng.annotations.BeforeMethod;
 
 import com.technocredits.ornghrm.base.PreRequisiteSteps;
 import com.technocredits.ornghrm.base.exceptionsClass.ElementNotEnabledException;
+import com.technocredits.ornghrm.pages.DashboardPage;
 import com.technocredits.ornghrm.pages.LoginPage;
+import com.technocredits.ornghrm.pages.MyInfoPage;
+import com.technocredits.ornghrm.pages.MyInfo_personalInfoPage;
 
-public class BaseTest{
+public class BaseTest {
 	
 	@BeforeMethod
 	public void commonTests() throws ElementNotEnabledException {
@@ -23,10 +26,33 @@ public class BaseTest{
 		
 		System.out.println("STEP- Login using valid credentials");
 		loginpage.performLogin("Admin", "SYpifJ2V2@");
+		
+		DashboardPage dashboard = goto_dashboard();
+		System.out.println("VERIFY- Dashboard is selected as default menu after login");
+		Assert.assertTrue(dashboard.isDashboardSelectedAsDefaultMenu(),"DashBoard is not the default menu");
+		
+		System.out.println("VERIFY- User profile is displayed");
+		Assert.assertTrue(dashboard.isUserProfileDisplayed(), "User profile is not present");	
+	}
+	
+	MyInfoPage goto_MyInfo() {
+		 MyInfoPage myinfo = new MyInfoPage();
+		 myinfo.clickOnMyInfo();
+		 return myinfo;
+	}
+	
+	MyInfo_personalInfoPage goto_PIPage() {
+		MyInfo_personalInfoPage piPage = new MyInfo_personalInfoPage();
+		return piPage;
+	}
+	
+	DashboardPage goto_dashboard() {
+		DashboardPage dashboard = new DashboardPage();
+		return dashboard;
 	}
 	
 	@AfterMethod
-	public void closeBrowser() {
+	public void tearDown() {
 		PreRequisiteSteps.tearDown();
 	}
 
